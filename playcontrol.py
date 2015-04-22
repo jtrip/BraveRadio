4b4fb3caa864ef5f021bf6c09987c9504dd405d1
root <root@beaglebone.(none)>
2015-04-22 03:02:47 +0000

    setup for testing

--- playcontrol.py
+++ playcontrol.py
@@ -4,30 +4,34 @@ import os
 from time import sleep, gmtime, strftime
 import Adafruit_BBIO.GPIO as GPIO
 
-PINS = ["","P9_14","P9_15","P9_18"]
+PINS = ["Debug","P9_16","P9_17","P9_18"]
+
+for pin in PINS:
+    GPIO.setup(pin, GPIO.IN)
 
 while True:
 
-  if(GPIO.input(PINS[1])==True and GPIO.input(PINS[2])==True):
-    if(GPIO.input(PINS[3])==False):
-      print(strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime()))
-      os.system('mpg321 /home/BraveRadio/audio/PardonMe.mp3 &')
-      time.sleep(3)
+    if(GPIO.input(PINS[1])==True and GPIO.input(PINS[2])==True):
+        if(GPIO.input(PINS[3])==False):
+            print(strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime()))
+            os.system('mpg321 /Audio/PardonMe.mp3 &')
+            sleep(3)
 
-  if(GPIO.input(PINS[1])==False and GPIO.input(PINS[2])==True):
-    if(GPIO.input(PINS[3])==False):
-      print(strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime()))
-      os.system('mpg321 /home/BraveRadio/audio/WeinerDog.mp3 &')
-      time.sleep(3)
+    if(GPIO.input(PINS[1])==False and GPIO.input(PINS[2])==True):
+        if(GPIO.input(PINS[3])==False):
+            print(strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime()))
+            os.system('mpg321 /Audio/WeinerDog.mp3 &')
+            sleep(3)
 
-  if(GPIO.input(PINS[2])==False and GPIO.input(PINS[1])==True):
-    if(GPIO.input(PINS[3])==False):
-      print(strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime()))
-      os.system('mpg321 /home/BraveRadio/audio/Soul_Injection.mp3 &')
-      time.sleep(3)
+    if(GPIO.input(PINS[2])==False and GPIO.input(PINS[1])==True):
+        if(GPIO.input(PINS[3])==False):
+            print(strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime()))
+            os.system('mpg321 /Audio/Soul_Injection.mp3 &')
+            sleep(3)
 
-  if(GPIO.input(PINS[1])==False and GPIO.input(PINS[2])==False):
-    if(GPIO.input(PINS[3])==False):
-      print(strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime()))
-      os.system('mpg321 /home/BraveRadio/audio/Codes.mp3 &')
-      time.sleep(3)
+    if(GPIO.input(PINS[1])==False and GPIO.input(PINS[2])==False):
+        if(GPIO.input(PINS[3])==False):
+            print(strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime()))
+            # os.system('mpg321 /Audio/Codes.mp3 &')
+            print('debug')
+            sleep(3)
